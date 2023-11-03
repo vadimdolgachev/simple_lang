873fc9d5872afc8cbe406211ff1db908462fec8b
vadimdolgachev <vadimdolgachev@gmail.com>
2023-11-03 20:42:08 +0700

    refactor

--- KaleidoscopeJIT.h
+++ KaleidoscopeJIT.h
@@ -28,77 +28,75 @@
 #include "llvm/IR/LLVMContext.h"
 #include <memory>
 
-namespace llvm {
-    namespace orc {
+namespace llvm::orc {
 
-        class KaleidoscopeJIT {
-        private:
-            std::unique_ptr<ExecutionSession> executionSession;
-            DataLayout dataLayout;
-            MangleAndInterner mangleAndInterpret;
-            RTDyldObjectLinkingLayer objectLinkingLayer;
-            IRCompileLayer compileLayer;
-            JITDylib &jitLib;
+    class KaleidoscopeJIT {
+    private:
+        std::unique_ptr<ExecutionSession> executionSession;
+        DataLayout dataLayout;
+        MangleAndInterner mangleAndInterpret;
+        RTDyldObjectLinkingLayer objectLinkingLayer;
+        IRCompileLayer compileLayer;
+        JITDylib &jitLib;
 
-        public:
-            KaleidoscopeJIT(std::unique_ptr<ExecutionSession> executionSession,
-                            JITTargetMachineBuilder targetMachineBuilder,
-                            const DataLayout &dataLayout)
-                    : executionSession(std::move(executionSession)),
-                      dataLayout(dataLayout),
-                      mangleAndInterpret(*executionSession, dataLayout),
-                      objectLinkingLayer(*executionSession,
-                                         []() { return std::make_unique<SectionMemoryManager>(); }),
-                      compileLayer(*executionSession, objectLinkingLayer,
-                                   std::make_unique<ConcurrentIRCompiler>(std::move(targetMachineBuilder))),
-                      jitLib(executionSession->createBareJITDylib("<main>")) {
-                jitLib.addGenerator(cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(
-                        dataLayout.getGlobalPrefix())));
-                if (targetMachineBuilder.getTargetTriple().isOSBinFormatCOFF()) {
-                    objectLinkingLayer.setOverrideObjectFlagsWithResponsibilityFlags(true);
-                    objectLinkingLayer.setAutoClaimResponsibilityForObjectSymbols(true);
-                }
+    public:
+        KaleidoscopeJIT(std::unique_ptr<ExecutionSession> executionSession,
+                        JITTargetMachineBuilder targetMachineBuilder,
+                        const DataLayout &dataLayout)
+                : executionSession(std::move(executionSession)),
+                  dataLayout(dataLayout),
+                  mangleAndInterpret(*this->executionSession, this->dataLayout),
+                  objectLinkingLayer(*this->executionSession,
+                                     []() { return std::make_unique<SectionMemoryManager>(); }),
+                  compileLayer(*this->executionSession, objectLinkingLayer,
+                               std::make_unique<ConcurrentIRCompiler>(std::move(targetMachineBuilder))),
+                  jitLib(this->executionSession->createBareJITDylib("<main>")) {
+            jitLib.addGenerator(cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(
+                    dataLayout.getGlobalPrefix())));
+            if (targetMachineBuilder.getTargetTriple().isOSBinFormatCOFF()) {
+                objectLinkingLayer.setOverrideObjectFlagsWithResponsibilityFlags(true);
+                objectLinkingLayer.setAutoClaimResponsibilityForObjectSymbols(true);
             }
+        }
 
-            ~KaleidoscopeJIT() {
-                if (auto Err = executionSession->endSession()) {
-                    executionSession->reportError(std::move(Err));
-                }
+        ~KaleidoscopeJIT() {
+            if (auto Err = executionSession->endSession()) {
+                executionSession->reportError(std::move(Err));
             }
+        }
 
-            static Expected<std::unique_ptr<KaleidoscopeJIT>> Create() {
-                auto EPC = SelfExecutorProcessControl::Create();
-                if (!EPC) {
-                    return EPC.takeError();
-                }
-                auto ES = std::make_unique<ExecutionSession>(std::move(*EPC));
-                JITTargetMachineBuilder JTMB(ES->getExecutorProcessControl().getTargetTriple());
-
-                auto DL = JTMB.getDefaultDataLayoutForTarget();
-                if (!DL) {
-                    return DL.takeError();
-                }
-                return std::make_unique<KaleidoscopeJIT>(std::move(ES), std::move(JTMB), std::move(*DL));
+        static Expected<std::unique_ptr<KaleidoscopeJIT>> Create() {
+            auto EPC = SelfExecutorProcessControl::Create();
+            if (!EPC) {
+                return EPC.takeError();
             }
+            auto ES = std::make_unique<ExecutionSession>(std::move(*EPC));
+            JITTargetMachineBuilder JTMB(ES->getExecutorProcessControl().getTargetTriple());
 
-            const DataLayout &getDataLayout() const { return dataLayout; }
-
-            JITDylib &getMainJITDylib() { return jitLib; }
-
-            Error addModule(ThreadSafeModule threadSafeModule,
-                            ResourceTrackerSP resTracker) {
-                if (resTracker == nullptr) {
-                    resTracker = jitLib.getDefaultResourceTracker();
-                }
-                return compileLayer.add(resTracker, std::move(threadSafeModule));
+            auto DL = JTMB.getDefaultDataLayoutForTarget();
+            if (!DL) {
+                return DL.takeError();
             }
+            return std::make_unique<KaleidoscopeJIT>(std::move(ES), std::move(JTMB), std::move(*DL));
+        }
 
-            Expected<ExecutorSymbolDef> lookup(const StringRef name) {
-                return executionSession->lookup({&jitLib}, mangleAndInterpret(name.str()));
+        const DataLayout &getDataLayout() const { return dataLayout; }
+
+        JITDylib &getMainJITDylib() { return jitLib; }
+
+        Error addModule(ThreadSafeModule threadSafeModule,
+                        ResourceTrackerSP resTracker) {
+            if (resTracker == nullptr) {
+                resTracker = jitLib.getDefaultResourceTracker();
             }
-        };
+            return compileLayer.add(resTracker, std::move(threadSafeModule));
+        }
 
-    } // end namespace orc
-} // end namespace llvm
+        Expected<ExecutorSymbolDef> lookup(const StringRef name) {
+            return executionSession->lookup({&jitLib}, mangleAndInterpret(name.str()));
+        }
+    };
+
+}  // namespace llvm::orc
 
 #endif // LLVM_EXECUTIONENGINE_ORC_KALEIDOSCOPEJIT_H
